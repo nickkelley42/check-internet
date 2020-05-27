ed7ec8429b2e66cb6c56e46dc62a1dbdd507c496
Nick <nick@nickkelleystuff.com>
2020-05-27 10:47:16 -0700

    basic functionality, hard-coded hosts

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 #!/usr/bin/env node
 
-function check_connection() {
+const ping = require('ping');
+
+async function check_connection() {
+	const hosts = [
+		'localhost',
+		'192.168.1.1',
+		'8.8.8.8',
+		'xkcd.com',
+	];
+	const resultsPromises = hosts.map(host => ping.promise.probe(host));
+	const results = await Promise.all(resultsPromises);
+	const success = results.every(res => res.alive);
+	if (!success) {
+		process.exitCode = 1;
+		for (let result of results) {
+			let status = result.alive ? 'success' : 'fail';
+			console.log(`${result.host}: ${status}`);
+		}
+	}
 }
 
 if (require.main === module) {
--- tests.js
+++ tests.js
@@ -1,8 +1,34 @@
 const qunit = require('qunit');
 const app = require('./index');
 
+class MockPing {
+	constructor() {
+		this.promise = {
+			probe(host) {
+				return Promise.resolve('done');
+			},
+		};
+	}
+}
+
 qunit.test('it exports a function when not run as main', function(assert) {
 	assert.equal(typeof app, 'function');
 });
 
 
+// skipping this for now; I'm not sure how to 
+// effectively mock the ping package yet.
+qunit.skip('it pings hosts', function(assert) {
+	assert.expect(1);
+	const ping = {
+		promise: {
+			probe(host) {
+				assert.step('calls probe with a host');
+				return Promise.resolve();
+			},
+		},
+	};
+	app();
+
+	assert.verifySteps(['calls probe with a host']);
+});
